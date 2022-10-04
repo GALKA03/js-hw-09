a0d1457d64f406079faa0d6fdf7565da35981851
Galina Matvienko <galyagmgm@gmail.com>
2022-10-04 16:50:22 +0200

    jj

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+console.log(startBtn)
+const stopBtn = document.querySelector('[data-stop]')
+console.log(stopBtn)
+let timerId = null;
 
+
+startBtn.addEventListener('click', onBtnClickStart);
+stopBtn.addEventListener('click', onBtnClickStop)
+
+
+function getRandomHexColor() {
+
+     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;   
+    
+  
+}
+
+function onBtnClickStart() {
+    const colorRandom = getRandomHexColor();
+    document.body.style.backgroundColor = colorRandom;
+  timerId = setInterval(() => {
+    console.log();
+  }, 1000);
+
+    }
+
+function onBtnClickStop() {
+clearInterval(timerId);
+  console.log(timerId);
+    }
